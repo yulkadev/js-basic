8c8c63bbff0c1e48a81b4f2690192f0d9487a128
lizarusi <lizarusi@gmail.com>
2019-09-20 12:00:00 +0300

    test

--- homework/lesson2/use-strict.js
+++ homework/lesson2/use-strict.js
@@ -3,4 +3,4 @@
 //Play with variable declaration try to declare it without reservation word (let/const/var) and
 //see what happen in case use-strict present or not (just delete top line and then set again)
 //and write as comment what will be in both cases
-// a = 'testing use strict';
+// a = 'testing use strict';   test
