4d0fa92cc839426311fc87cac08273874e2954be
Yuliia <yakimovayuliya@i.ua>
2019-10-07 17:07:00 +0300

    Do datatypes homework

---
+++ Exercises/data-types-overview/data-types-overview.js
@@ -0,0 +1,27 @@
+//1. Declare a variable on each type using two options: plain creation, creation with constructor.
+
+let myNumber = 10;
+let myString = "My name is Yuliia";
+let myBoolean = true;
+let myNull = null;
+let myUndefined = undefined;
+let MySymbol = Symbol();
+let MyObject = {job:"QA"};
+
+//2. Use operator typeof on each of them.
+
+console.log(typeof myNumber);
+console.log(typeof myString);
+console.log(typeof myBoolean);
+console.log(typeof myNull);
+console.log(typeof myUndefined);
+console.log(typeof MySymbol);
+console.log(typeof MyObject);
+
+//3. Demonstrate object's references.
+//3. Demonstrate string's backticks features.
+
+# Additional
+
+//1. Declare an array containing different data types.
+//2. Iterate through it and collect amount of each data type to object.
