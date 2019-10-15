52a7f88a22c6123ae92199f2d5834412aac91158
Yuliia <yakimovayuliya@i.ua>
2019-10-15 16:21:53 +0300

    Add homework without additional task

---
+++ Exercises/strings/strings.js
@@ -0,0 +1,37 @@
+
+//Assign string with unicode to a variable.
+
+const varWithUnicode = '\u00A9';
+console.log(varWithUnicode);
+
+//Declare a variable with string with escaped symbols.
+
+let varWithEscapedSymbols = 'It\'s not a problem.'
+console.log(varWithEscapedSymbols);
+
+//Declare a variable which will contain name of your favourite animal.
+
+let myFavoriteAnimalName = 'Tom';
+
+//Log a message My favourite animal is with dynamic variable from previous task.
+
+console.log(`My favourite naimal name is ${myFavoriteAnimalName}`);
+
+//Declare a variable which contain name and surname of your favourite writer in lowercase. Log his/her name with capitals first letters.
+
+const myFavouriteWriter = 'conan doyle';
+
+function capitalize(author) {  
+    return author.split(' ').map(el => el.charAt(0).toUpperCase() + el.substring(1)).join(' ');  
+}  
+
+console.log(capitalize(myFavouriteWriter));
+
+//Additional
+
+//Declare a function which will receive password string. Check is password strong enough:
+//should contain at least one capital letter;
+//length no less than 8 characters.
+//should contain at leas one special character.
+//Make function which will generate password suggestion based on passed password, and required strongest.
+
