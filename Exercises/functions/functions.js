ea2aaa145fbe9fe57188a971cd9ab0169776585c
Yuliia <yakimovayuliya@i.ua>
2019-11-05 18:53:04 +0200

    Add part of functions homework

--- Exercises/functions/functions.js
+++ Exercises/functions/functions.js
@@ -1,3 +1,24 @@
-function takeMyParametersAmount(a, b, c) {
-  return null;
+//1. Create a function declaration, function expression, lambda function, and using function object.
+
+function funcDeclaration() {
+  console.log('We declared a funcxtion using this code');
 }
+
+const myFuncExpression = function (parameter1) { return parameter1; };
+
+const arrowFunc = (param1) => { console.log(param1); };
+
+const myFunctionObject = new Function(console.log('Hello'));
+
+//2. Get parameters amount of function `takeMyParametersAmount`.
+
+function takeMyParametersAmount(...newArr) {
+  return newArr.length;
+}
+
+//2. Create a function and log all passed parameters to it. Check the `length` of this function.
+
+function logAllParameters(a, b, c) {
+
+}
+//3. Using anonymous function encapsulate function body from global scope.
