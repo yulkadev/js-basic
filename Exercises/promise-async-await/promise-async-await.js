ff029af0d55376c4ff822d423f01f68a7255a845
Yuliia <yakimovayuliya@i.ua>
2020-01-03 17:31:43 +0200

    Do first part of promises homework

--- Exercises/promise-async-await/promise-async-await.js
+++ Exercises/promise-async-await/promise-async-await.js
@@ -1,3 +1,12 @@
+//# Promise/Async/Await
+
+//1. Make function which will return promise which will resolve in passed to function amount of milliseconds.
+//2. Using `fakeAjax` do 3 parallel requests.
+//3. Catch errors but only for failed executions.
+//4. Call `fakeAjax` do 3 request in serial.
+//5. Using your `delay` function from first task add delay between each serial request.
+
+
 const fakeAjax = async () => {
   if (Math.random() > 0.5) {
     return { status: 200 };
@@ -5,3 +14,17 @@ const fakeAjax = async () => {
 
   throw new Error('Server error!');
 };
+
+//1. Make function which will return promise which will resolve in passed to function amount of milliseconds.
+
+async function resolveWithSetTimeout(ms) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('done!'), ms);
+  });
+
+  const result = await promise;
+
+  console.log(result);
+}
+
+resolveWithSetTimeout(3000);
