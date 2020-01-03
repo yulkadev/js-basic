6adeefe87d366ab9d42f4a01289eafac3c687c3e
Yuliia <yakimovayuliya@i.ua>
2020-01-03 18:34:04 +0200

    Add second task to promises homework

--- Exercises/promise-async-await/promise-async-await.js
+++ Exercises/promise-async-await/promise-async-await.js
@@ -1,19 +1,7 @@
-//# Promise/Async/Await
 
-//1. Make function which will return promise which will resolve in passed to function amount of milliseconds.
-//2. Using `fakeAjax` do 3 parallel requests.
 //3. Catch errors but only for failed executions.
 //4. Call `fakeAjax` do 3 request in serial.
 //5. Using your `delay` function from first task add delay between each serial request.
-
-
-const fakeAjax = async () => {
-  if (Math.random() > 0.5) {
-    return { status: 200 };
-  }
-
-  throw new Error('Server error!');
-};
 
 //1. Make function which will return promise which will resolve in passed to function amount of milliseconds.
 
@@ -28,3 +16,23 @@ async function resolveWithSetTimeout(ms) {
 }
 
 resolveWithSetTimeout(3000);
+
+//2. Using `fakeAjax` do 3 parallel requests.
+
+const fakeAjax = async () => {
+  if (Math.random() > 0.5) {
+    return { status: 200 };
+  }
+  throw new Error('Server error!');
+};
+
+async function parallel() {
+  console.log('PARALLEL with await Promise.all');
+  await Promise.all([
+      (async()=>console.log(await fakeAjax()))(),
+      (async()=>console.log(await fakeAjax()))(),
+      (async()=>console.log(await fakeAjax()))()
+  ]);
+}
+
+parallel();
